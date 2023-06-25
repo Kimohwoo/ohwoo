36c63d711f678ce760f91b662eb5821fb1accb2e
Kimohwoo <kowkow292@naver.com>
2023-06-25 19:25:07 +0900

    addUesr GetMapping 및 JWT토큰 관련 작업 중 현재 Security 관련 작업은 다 끝났다고 보면 되고 JWT 토큰까지 포함해서 작업 할 예정 User단의 컨트롤러 RestController로 변경 IDE도 Intellij로 변경

--- src/main/webapp/resources/All.sql
+++ src/main/webapp/resources/All.sql
@@ -1,35 +1,32 @@
-CREATE DATABASE OHWOODB;
-
 USE ohwoodb;
 
 DROP TABLE user;
 DROP TABLE user_auth;
-DROP TABLE admin;
+DROP TABLE ban;
 DROP TABLE board;
 DROP TABLE file;
 DROP TABLE likes;
+DROP TABLE images;
 
 CREATE TABLE user(
    id VARCHAR(50) PRIMARY KEY,
    password VARCHAR(100),
    name VARCHAR(10),
-   
    nickname VARCHAR(10),
-   phone VARCHAR(13),
+   phone CHAR(11),
    address VARCHAR(50),
-   level VARCHAR(5),
+   level VARCHAR(5) UNIQUE,
    regdate DATE
 );
 
-CREATE TABLE admin(
-   id VARCHAR(50) PRIMARY KEY,
-   password VARCHAR(100),
-   name VARCHAR(10),
-   phone VARCHAR(13),
-   address VARCHAR(50),
-   regdate DATE
+CREATE TABLE ban(
+	id VARCHAR(50),
+    level VARCHAR(5),
+    count int,
+    date DATE,
+    CONSTRAINT banfk_id_useridpk FOREIGN KEY(id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT banfk_level_userlevel FOREIGN KEY(level) REFERENCES user(level) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
 
 CREATE TABLE user_auth(
 	id VARCHAR(50),
@@ -40,16 +37,23 @@ CREATE TABLE user_auth(
 CREATE TABLE board(
    no BIGINT AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR(50),
-   author VARCHAR(10),
+   author VARCHAR(50),
    content VARCHAR(500),
    regdate DATE,
    updateday DATE,
    CONSTRAINT brdfk_author_user_id FOREIGN KEY(author) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE file(
+CREATE TABLE images(
 	no BIGINT AUTO_INCREMENT PRIMARY KEY,
-    id VARCHAR(20),
+    id VARCHAR(50),
+    src VARCHAR(50),
+    CONSTRAINT imgfk_id_user_id FOREIGN KEY(id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE files(
+	no BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id VARCHAR(50),
     src VARCHAR(50),
     CONSTRAINT filefk_id_user_id FOREIGN KEY(id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -57,22 +61,16 @@ CREATE TABLE file(
 CREATE TABLE likes(
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
     no BIGINT,
-    uid VARCHAR(20),
+    uid VARCHAR(50),
     CONSTRAINT likefk_no_board_no FOREIGN KEY(no) REFERENCES board(no) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT likefk_uid_user_id FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+COMMIT;
 
 SELECT * FROM user;
+SELECT * FROM user_auth;
 INSERT INTO user_auth(id, auth)
 VALUES ('1111', 'ROLE_USER');
-
-종속 삭제 방법
-SET foreign_key_checks = 0;
-drop table board;
-SET foreign_key_checks = 1;
-
-//보드 increment 초기화
-ALTER TABLE board AUTO_INCREMENT = 1;
 
 COMMIT;
